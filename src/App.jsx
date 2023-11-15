96bc3fd741bcffdbb5ba4cb163298d2171e3df40
ross402 <65184393+ross402@users.noreply.github.com>
2023-11-15 13:24:56 -0500

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Header from "./Components/Header/Header";
 import Profile from "./Components/Profile/Profile";
 import About from "./Components/About/About";
@@ -9,6 +10,9 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>Ross Portfolio</title>
+      </Helmet>
       <Header />
       <Routes>
         <Route path="/" element={<Profile />} />
